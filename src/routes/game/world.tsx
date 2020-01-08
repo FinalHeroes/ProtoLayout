1ec82c0d7a2d4f7805712861660f8fbbdb0b5f31
Daniel Grondin <king@numsgil.co>
2020-01-08 14:24:18 -0500

    The map now has dragging! (PROTOTYPE V1)

--- src/routes/game/world.tsx
+++ src/routes/game/world.tsx
@@ -1,8 +1,12 @@
-import {createElement, Fragment, FunctionComponent, useEffect, useRef} from "react";
+import {createElement, Fragment, FunctionComponent, MouseEvent, useEffect, useRef, useState} from "react";
 import {Card, CardContent, CardHeader, Grid, Typography} from "@material-ui/core";
 
 const World: FunctionComponent = () => {
 	let canvas = useRef<HTMLCanvasElement>(null);
+	const [zoom, setZoom] = useState(1.0);
+	const [offsetX, setOffsetX] = useState(0);
+	const [offsetY, setOffsetY] = useState(0);
+	const [dragging, setDragging] = useState(false);
 
 	const render = (canvas: HTMLCanvasElement) => {
 		const rect = canvas.getBoundingClientRect();
@@ -12,14 +16,51 @@ const World: FunctionComponent = () => {
 		const ctx = canvas.getContext("2d");
 		if (ctx) {
 			ctx.clearRect(0, 0, rect.width, rect.height);
-			ctx.strokeStyle = "black";
-			ctx.lineWidth = 1;
 
+			ctx.save();
+			ctx.scale(zoom, zoom);
+			ctx.translate(offsetX, offsetY);
+
+			ctx.beginPath();
 			for (let y = 0; y < 32; ++y) {
 				for (let x = 0; x < 40; ++x) {
-					ctx.strokeRect(x * 32, y * 32, 32, 32);
+					ctx.rect(x * 32, y * 32, 32, 32);
 				}
 			}
+
+			ctx.strokeStyle = "black";
+			ctx.lineWidth = 0.5;
+			ctx.stroke();
+			ctx.restore();
+		}
+	};
+
+	const mouseDown = (e: MouseEvent<HTMLCanvasElement>) => {
+		if (e.button === 0 && e.ctrlKey) {
+			setDragging(true);
+			console.log("Dragging started");
+		}
+	};
+
+	const mouseUp = (e: MouseEvent<HTMLCanvasElement>) => {
+		if (e.button === 0 && e.ctrlKey) {
+			setDragging(false);
+			console.log("Dragging ended");
+		}
+	};
+
+	const mouseMove = (e: MouseEvent<HTMLCanvasElement>) => {
+		if (e.button === 0 && e.ctrlKey && canvas.current && dragging) {
+			setOffsetX(offsetX + e.movementX);
+			setOffsetY(offsetY + e.movementY);
+			render(canvas.current);
+		}
+	};
+
+	const click =  (e: MouseEvent<HTMLCanvasElement>) => {
+		e.preventDefault();
+		if (e.button === 0 && !e.ctrlKey && !dragging) {
+			console.log("CLICKED!");
 		}
 	};
 
@@ -47,7 +88,10 @@ const World: FunctionComponent = () => {
 				<Card raised>
 					<CardHeader title="World"/>
 					<CardContent style={{padding: 0}}>
-						<canvas ref={canvas} style={{width: "100%", height: 400}}/>
+						<canvas ref={canvas} style={{width: "100%", height: 400}}
+						        onMouseLeave={() => setDragging(false)} onClick={click}
+						        onMouseDown={mouseDown} onMouseUp={mouseUp} onMouseMove={mouseMove}
+						/>
 					</CardContent>
 				</Card>
 			</Grid>
