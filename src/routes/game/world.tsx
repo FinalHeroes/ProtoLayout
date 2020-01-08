0e929cf42547cc0d504b70acd88af95fb6bb9945
Daniel Grondin <king@numsgil.co>
2020-01-08 11:28:08 -0500

    Made the canvas a little bit more responsive... awesomo!

--- src/routes/game/world.tsx
+++ src/routes/game/world.tsx
@@ -3,13 +3,42 @@ import {Card, CardContent, CardHeader, Grid, Typography} from "@material-ui/core
 
 const World: FunctionComponent = () => {
 	let canvas = useRef<HTMLCanvasElement>(null);
-	let content = useRef<HTMLDivElement>(null);
+
+	const render = (canvas: HTMLCanvasElement) => {
+		const rect = canvas.getBoundingClientRect();
+		canvas.width = rect.width;
+		canvas.height = rect.height;
+
+		const ctx = canvas.getContext("2d");
+		if (ctx) {
+			ctx.clearRect(0, 0, rect.width, rect.height);
+			ctx.strokeStyle = "black";
+			ctx.lineWidth = 1;
+
+			for (let y = 0; y < 32; ++y) {
+				for (let x = 0; x < 40; ++x) {
+					ctx.strokeRect(x * 32, y * 32, 32, 32);
+				}
+			}
+		}
+	};
+
+	const onResize = () => {
+		if (canvas.current) {
+			render(canvas.current);
+		}
+	};
 
 	useEffect(() => {
 		console.log("Home rendered");
-		if (content.current && canvas.current) {
-			canvas.current.width = content.current.clientWidth;
+		if (canvas.current) {
+			render(canvas.current);
+
+			window.addEventListener("resize", onResize);
+			return () => window.removeEventListener("resize", onResize);
 		}
+
+		return () => {};
 	}, []);
 
 	return <Fragment>
@@ -17,8 +46,8 @@ const World: FunctionComponent = () => {
 			<Grid item lg={9}>
 				<Card raised>
 					<CardHeader title="World"/>
-					<CardContent ref={content} style={{padding: 0}}>
-						<canvas ref={canvas} height={400}/>
+					<CardContent style={{padding: 0}}>
+						<canvas ref={canvas} style={{width: "100%", height: 400}}/>
 					</CardContent>
 				</Card>
 			</Grid>
