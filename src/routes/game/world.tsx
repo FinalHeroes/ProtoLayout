401cf72a029d95c9d7d3e3eb55c33b60db68b2fc
Daniel Grondin <king@numsgil.co>
2020-01-08 20:10:24 -0500

    The map now has controls for navigation mode and zoom! (PROTOTYPE V2)

--- src/routes/game/world.tsx
+++ src/routes/game/world.tsx
@@ -1,12 +1,49 @@
 import {createElement, Fragment, FunctionComponent, MouseEvent, useEffect, useRef, useState} from "react";
-import {Card, CardContent, CardHeader, Grid, Typography} from "@material-ui/core";
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	createStyles,
+	FormControlLabel,
+	FormGroup,
+	Grid,
+	makeStyles,
+	Mark,
+	Slider,
+	Switch,
+	Theme
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		zoomLabel: {
+			marginRight: 25,
+		},
+		zoomGroup: {
+			width: 240,
+			marginRight: 20,
+		},
+		mapCard: {
+			padding: 0,
+			["&:last-child"]: {
+				paddingBottom: 0,
+			},
+		},
+		canvas: {
+			width: "100%",
+			height: 400,
+		},
+	}),
+);
 
 const World: FunctionComponent = () => {
 	let canvas = useRef<HTMLCanvasElement>(null);
+	const classes = useStyles();
 	const [zoom, setZoom] = useState(1.0);
 	const [offsetX, setOffsetX] = useState(0);
 	const [offsetY, setOffsetY] = useState(0);
 	const [dragging, setDragging] = useState(false);
+	const [navMode, setNavMode] = useState(false);
 
 	const render = (canvas: HTMLCanvasElement) => {
 		const rect = canvas.getBoundingClientRect();
@@ -36,30 +73,30 @@ const World: FunctionComponent = () => {
 	};
 
 	const mouseDown = (e: MouseEvent<HTMLCanvasElement>) => {
-		if (e.button === 0 && e.ctrlKey) {
+		if (e.button === 0 && navMode) {
 			setDragging(true);
 			console.log("Dragging started");
 		}
 	};
 
 	const mouseUp = (e: MouseEvent<HTMLCanvasElement>) => {
-		if (e.button === 0 && e.ctrlKey) {
+		if (e.button === 0 && navMode) {
 			setDragging(false);
 			console.log("Dragging ended");
 		}
 	};
 
 	const mouseMove = (e: MouseEvent<HTMLCanvasElement>) => {
-		if (e.button === 0 && e.ctrlKey && canvas.current && dragging) {
+		if (e.button === 0 && navMode && canvas.current && dragging) {
 			setOffsetX(offsetX + e.movementX);
 			setOffsetY(offsetY + e.movementY);
 			render(canvas.current);
 		}
 	};
 
-	const click =  (e: MouseEvent<HTMLCanvasElement>) => {
+	const click = (e: MouseEvent<HTMLCanvasElement>) => {
 		e.preventDefault();
-		if (e.button === 0 && !e.ctrlKey && !dragging) {
+		if (e.button === 0 && !navMode && !dragging) {
 			console.log("CLICKED!");
 		}
 	};
@@ -82,13 +119,56 @@ const World: FunctionComponent = () => {
 		return () => {};
 	}, []);
 
+	const marks: Mark[] = [
+		{value: 0.5, label: "50%"},
+		{value: 1.0, label: "100%"},
+		{value: 1.5, label: "150%"},
+	];
+
 	return <Fragment>
 		<Grid container alignItems="center" justify="center">
 			<Grid item lg={9}>
 				<Card raised>
-					<CardHeader title="World"/>
-					<CardContent style={{padding: 0}}>
-						<canvas ref={canvas} style={{width: "100%", height: 400}}
+					<CardHeader title="World" action={
+						<FormGroup row>
+							<FormControlLabel label="Zoom" labelPlacement="start"
+							                  classes={{root: classes.zoomGroup, label: classes.zoomLabel}}
+							                  control={
+								                  <Slider
+									                  color="secondary"
+									                  min={0.5}
+									                  max={1.5}
+									                  step={0.1}
+									                  marks={marks}
+									                  valueLabelDisplay="off"
+									                  value={zoom}
+									                  onChange={(e, value) => {
+										                  if (typeof value === "number" && canvas.current) {
+											                  setZoom(value);
+											                  render(canvas.current);
+										                  }
+									                  }}
+								                  />
+							                  }
+							/>
+							<FormControlLabel
+								label="Navigation Mode"
+								labelPlacement="start"
+								control={
+									<Switch
+										color="secondary"
+										value={navMode}
+										onChange={e => {
+											setNavMode(e.target.checked);
+											setDragging(false);
+										}}
+									/>}
+							/>
+						</FormGroup>
+					}
+					/>
+					<CardContent classes={{root: classes.mapCard}}>
+						<canvas ref={canvas} className={classes.canvas}
 						        onMouseLeave={() => setDragging(false)} onClick={click}
 						        onMouseDown={mouseDown} onMouseUp={mouseUp} onMouseMove={mouseMove}
 						/>
