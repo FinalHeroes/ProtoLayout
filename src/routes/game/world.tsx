8208d79b02bfb90bc966765e32d3911b0ea18745
Daniel Grondin <king@numsgil.co>
2020-01-10 09:22:38 -0500

    Added raise on hover and added encounter area and 2 info cards

--- src/routes/game/world.tsx
+++ src/routes/game/world.tsx
@@ -1,6 +1,7 @@
 import {createElement, Fragment, FunctionComponent, MouseEvent, useEffect, useRef, useState} from "react";
 import {
 	Card,
+	CardActionArea,
 	CardContent,
 	CardHeader,
 	createStyles,
@@ -11,7 +12,8 @@ import {
 	Mark,
 	Slider,
 	Switch,
-	Theme
+	Theme,
+	Typography
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -29,6 +31,15 @@ const useStyles = makeStyles((theme: Theme) =>
 				paddingBottom: 0,
 			},
 		},
+		infoCard: {
+			padding: 5,
+			["&:last-child"]: {
+				paddingBottom: 5,
+			},
+		},
+		infoText: {
+			margin: 0,
+		},
 		canvas: {
 			width: "100%",
 			height: 400,
@@ -36,7 +47,7 @@ const useStyles = makeStyles((theme: Theme) =>
 	}),
 );
 
-const World: FunctionComponent = () => {
+const MapCard: FunctionComponent = () => {
 	let canvas = useRef<HTMLCanvasElement>(null);
 	const classes = useStyles();
 	const [zoom, setZoom] = useState(1.0);
@@ -44,6 +55,7 @@ const World: FunctionComponent = () => {
 	const [offsetY, setOffsetY] = useState(0);
 	const [dragging, setDragging] = useState(false);
 	const [navMode, setNavMode] = useState(false);
+	const [raised, setRaised] = useState(false);
 
 	const render = (canvas: HTMLCanvasElement) => {
 		const rect = canvas.getBoundingClientRect();
@@ -126,51 +138,110 @@ const World: FunctionComponent = () => {
 		{value: 1.5, label: "150%"},
 	];
 
-	return <Fragment>
-		<Grid container alignItems="center" justify="center">
-			<Grid item lg={9}>
-				<Card raised>
-					<CardHeader title="World" action={
-						<FormGroup row>
-							<FormControlLabel label="Zoom" labelPlacement="start"
-							                  classes={{root: classes.zoomGroup, label: classes.zoomLabel}}
-							                  control={
-								                  <Slider
-									                  color="secondary" min={0.5} max={1.5} step={0.1}
-									                  marks={marks} valueLabelDisplay="off" value={zoom}
-									                  onChange={(e, value) => {
-										                  if (typeof value === "number" && canvas.current) {
-											                  setZoom(value);
-											                  render(canvas.current);
-										                  }
-									                  }}
-								                  />
-							                  }
-							/>
-							<FormControlLabel
-								label="Navigation Mode"
-								labelPlacement="start"
-								control={
-									<Switch
-										color="secondary" value={navMode}
-										onChange={e => {
-											setNavMode(e.target.checked);
-											setDragging(false);
-										}}
-									/>
-								}
-							/>
-						</FormGroup>
-					}
+	return <Card raised={raised}>
+		<CardActionArea onMouseEnter={() => setRaised(true)} onMouseLeave={() => setRaised(false)} disableRipple>
+			<CardHeader title="World" action={
+				<FormGroup row>
+					<FormControlLabel label="Zoom" labelPlacement="start"
+					                  classes={{root: classes.zoomGroup, label: classes.zoomLabel}}
+					                  control={
+						                  <Slider
+							                  color="secondary" min={0.5} max={1.5} step={0.1}
+							                  marks={marks} valueLabelDisplay="off" value={zoom}
+							                  onChange={(e, value) => {
+								                  if (typeof value === "number" && canvas.current) {
+									                  setZoom(value);
+									                  render(canvas.current);
+								                  }
+							                  }}
+						                  />
+					                  }
 					/>
-					<CardContent classes={{root: classes.mapCard}}>
-						<canvas
-							ref={canvas} className={classes.canvas}
-							onMouseLeave={() => setDragging(false)} onClick={click}
-							onMouseDown={mouseDown} onMouseUp={mouseUp} onMouseMove={mouseMove}
-						/>
-					</CardContent>
-				</Card>
+					<FormControlLabel
+						label="Navigation Mode"
+						labelPlacement="start"
+						control={
+							<Switch
+								color="secondary" value={navMode}
+								onChange={e => {
+									setNavMode(e.target.checked);
+									setDragging(false);
+								}}
+							/>
+						}
+					/>
+				</FormGroup>
+			}
+			/>
+			<CardContent classes={{root: classes.mapCard}}>
+				<canvas
+					ref={canvas} className={classes.canvas}
+					onMouseLeave={() => setDragging(false)} onClick={click}
+					onMouseDown={mouseDown} onMouseUp={mouseUp} onMouseMove={mouseMove}
+				/>
+			</CardContent>
+		</CardActionArea>
+	</Card>;
+};
+
+const World: FunctionComponent = () => {
+	const classes = useStyles();
+	const [raised, setRaised] = useState({
+		location: false,
+		players: false,
+	});
+
+	return <Fragment>
+		<Grid container alignItems="center" justify="center" spacing={2}>
+			<Grid item lg={9}>
+				<MapCard/>
+			</Grid>
+			<Grid container item lg={9} spacing={1}>
+				<Grid item lg={8}>
+					<Card style={{height: "100%"}}/>
+				</Grid>
+				<Grid container item lg={4} direction="column" spacing={1}>
+					<Grid item lg>
+						<Card raised={raised.location}>
+							<CardActionArea
+								onMouseEnter={() => setRaised({
+									location: true,
+									players: false,
+								})}
+								onMouseLeave={() => setRaised({
+									location: false,
+									players: false,
+								})}
+							>
+								<CardContent classes={{root: classes.infoCard}}>
+									<Typography paragraph className={classes.infoText}>
+										Location : Isandiel @ 1.1
+									</Typography>
+								</CardContent>
+							</CardActionArea>
+						</Card>
+					</Grid>
+					<Grid item lg>
+						<Card raised={raised.players}>
+							<CardActionArea
+								onMouseEnter={() => setRaised({
+									location: false,
+									players: true
+								})}
+								onMouseLeave={() => setRaised({
+									location: false,
+									players: false
+								})}
+							>
+								<CardContent classes={{root: classes.infoCard}}>
+									<Typography paragraph className={classes.infoText}>
+										There are 37 players here
+									</Typography>
+								</CardContent>
+							</CardActionArea>
+						</Card>
+					</Grid>
+				</Grid>
 			</Grid>
 		</Grid>
 	</Fragment>;
