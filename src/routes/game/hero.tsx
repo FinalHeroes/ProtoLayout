4fe565e0ac74079c194eca0e8a006771fddf002d
Daniel Grondin <king@numsgil.co>
2020-01-10 10:49:50 -0500

    Forced equipment slot to be 128x128 and added raise on hove to them

--- src/routes/game/hero.tsx
+++ src/routes/game/hero.tsx
@@ -1,106 +1,109 @@
-import {createElement, FunctionComponent} from "react";
-import {Card, CardContent, CardHeader, Grid, Typography} from "@material-ui/core";
+import {createElement, FunctionComponent, useState} from "react";
+import {
+	Card,
+	CardActionArea,
+	CardContent,
+	CardHeader,
+	createStyles,
+	Grid,
+	makeStyles,
+	Theme,
+	Typography
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		itemSlotCard: {
+			width: 128,
+			height: 128,
+		},
+		itemSlotAction: {
+			width: "100%",
+			height: "100%",
+		},
+	}),
+);
+
+interface EquipmentSlotProps {
+	name: string;
+}
+
+const EquipmentSlot: FunctionComponent<EquipmentSlotProps> = props => {
+	const {name} = props;
+	const classes = useStyles();
+	const [raised, setRaised] = useState(false);
+
+	return <Card raised={raised} classes={{root: classes.itemSlotCard}}>
+		<CardActionArea
+			classes={{root: classes.itemSlotAction}}
+			onMouseEnter={() => setRaised(true)}
+			onMouseLeave={() => setRaised(false)}
+		>
+			<CardContent>
+				<Typography paragraph>{name}</Typography>
+			</CardContent>
+		</CardActionArea>
+	</Card>
+};
 
 const Hero: FunctionComponent = () => {
+	const classes = useStyles();
+
 	return <Grid container justify="center">
 		<Grid item lg={9}>
 			<Card raised>
 				<CardHeader title="Hero"/>
 				<CardContent>
 					<Grid container spacing={2}>
-						<Grid container item direction="row" spacing={2}>
-							<Grid container item lg={3} direction="column" justify="flex-start" spacing={3}>
+						<Grid container item direction="row" spacing={2} justify="center">
+							<Grid container item lg={2} direction="column" spacing={2}>
 								<Grid item>
-									<Card>
-										<CardContent>
-											<Typography paragraph>Head</Typography>
-										</CardContent>
-									</Card>
+									<EquipmentSlot name="Head"/>
 								</Grid>
 								<Grid item>
-									<Card>
-										<CardContent>
-											<Typography paragraph>Chest</Typography>
-										</CardContent>
-									</Card>
+									<EquipmentSlot name="Chest"/>
 								</Grid>
 								<Grid item>
-									<Card>
-										<CardContent>
-											<Typography paragraph>Belt</Typography>
-										</CardContent>
-									</Card>
+									<EquipmentSlot name="Belt"/>
 								</Grid>
 								<Grid item>
-									<Card>
-										<CardContent>
-											<Typography paragraph>Boot</Typography>
-										</CardContent>
-									</Card>
+									<EquipmentSlot name="Boot"/>
 								</Grid>
 							</Grid>
 
-							<Grid item lg={6}>
-								<Card style={{height: "100%"}}>
+							<Grid item lg={8}>
+								<Card style={{height: "100%"}} variant="outlined">
 									<CardContent>
 										<Typography paragraph>Hero Picture Here</Typography>
 									</CardContent>
 								</Card>
 							</Grid>
 
-							<Grid container item lg={3} direction="column" justify="flex-start" spacing={3}>
+							<Grid container item lg={2} direction="column" justify="flex-start" spacing={2}>
 								<Grid item>
-									<Card>
-										<CardContent>
-											<Typography paragraph>Left Hand</Typography>
-										</CardContent>
-									</Card>
+									<EquipmentSlot name="Left Hand"/>
 								</Grid>
 								<Grid item>
-									<Card>
-										<CardContent>
-											<Typography paragraph>Right Hand</Typography>
-										</CardContent>
-									</Card>
+									<EquipmentSlot name="Right Hand"/>
 								</Grid>
 								<Grid item>
-									<Card>
-										<CardContent>
-											<Typography paragraph>Ring 1</Typography>
-										</CardContent>
-									</Card>
+									<EquipmentSlot name="Ring 1"/>
 								</Grid>
 								<Grid item>
-									<Card>
-										<CardContent>
-											<Typography paragraph>Ring 2</Typography>
-										</CardContent>
-									</Card>
+									<EquipmentSlot name="Ring 2"/>
 								</Grid>
 							</Grid>
 						</Grid>
 
 						<Grid container item lg={12} justify="space-evenly">
 							<Grid item lg={2}>
-								<Card>
-									<CardContent>
-										<Typography paragraph>Neck</Typography>
-									</CardContent>
-								</Card>
+								<EquipmentSlot name="Neck"/>
 							</Grid>
 							<Grid item lg={2}>
-								<Card>
-									<CardContent>
-										<Typography paragraph>Bag</Typography>
-									</CardContent>
-								</Card>
+								<EquipmentSlot name="Bag"/>
 							</Grid>
 							<Grid item lg={2}>
-								<Card>
-									<CardContent>
-										<Typography paragraph>Artifact</Typography>
-									</CardContent>
-								</Card>
+								<EquipmentSlot name="Artifact"/>
 							</Grid>
 						</Grid>
 					</Grid>
