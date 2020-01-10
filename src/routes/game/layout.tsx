8100643301c0cfcfd904fdd175dbedc125edec35
Daniel Grondin <king@numsgil.co>
2020-01-10 10:25:37 -0500

    Added a chat FAB with its chat card

--- src/routes/game/layout.tsx
+++ src/routes/game/layout.tsx
@@ -1,5 +1,22 @@
-import {AppBar, Button, createStyles, makeStyles, Theme, Toolbar, Typography} from "@material-ui/core";
-import {createElement, Fragment, FunctionComponent} from "react";
+import {
+	AppBar,
+	Button,
+	Card,
+	CardActions,
+	CardContent,
+	CardHeader,
+	createStyles,
+	Fab,
+	IconButton,
+	makeStyles,
+	TextField,
+	Theme,
+	Toolbar,
+	Typography,
+	Zoom
+} from "@material-ui/core";
+import {createElement, Fragment, FunctionComponent, useState} from "react";
+import {ChatSharp, CloseSharp} from "@material-ui/icons";
 import {useLinkProps} from "react-navi";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -17,7 +34,23 @@ const useStyles = makeStyles((theme: Theme) =>
 		appBarLabel: {
 			marginLeft: theme.spacing(2),
 			marginRight: theme.spacing(2),
-		}
+		},
+		chat: {
+			position: "absolute",
+			bottom: theme.spacing(2),
+			right: theme.spacing(2),
+		},
+		chatContent: {
+			maxHeight: "20rem",
+			maxWidth: "18rem",
+			height: "20rem",
+			width: "18rem",
+			overflow: "auto",
+			padding: 8,
+		},
+		chatParagraph: {
+			marginBottom: 8,
+		},
 	}),
 );
 
@@ -37,8 +70,21 @@ const AppBarLink: FunctionComponent<AppBarLinkProps> = props => {
 	</Button>
 };
 
+interface ChatMessage {
+	sender: string;
+	content: string;
+}
+
 const GameLayout: FunctionComponent = props => {
 	const classes = useStyles();
+	const [chatVisible, setChatVisible] = useState(false);
+
+	const messages: ChatMessage[] = [
+		{sender: "Stormist", content: "I'm level 98! What about you?"},
+		{sender: "SpikyPigeon", content: "I'm struggling with that freaking boss..."},
+		{sender: "Stormist", content: "Yeah... He's a bitch"},
+		{sender: "System", content: "Hello World!"},
+	];
 
 	return <Fragment>
 		<AppBar position="fixed" className={classes.appBar}>
@@ -55,6 +101,35 @@ const GameLayout: FunctionComponent = props => {
 		<main className={classes.content}>
 			<div className={classes.toolbar}/>
 			{props.children || null}
+
+			<Zoom in={!chatVisible}>
+				<Fab
+					className={classes.chat} color="primary" centerRipple
+					onClick={() => setChatVisible(true)}
+				>
+					<ChatSharp/>
+				</Fab>
+			</Zoom>
+			<Zoom in={chatVisible}>
+				<Card raised classes={{root: classes.chat}}>
+					<CardHeader title="Chat" action={
+						<IconButton onClick={() => setChatVisible(false)} color="secondary">
+							<CloseSharp/>
+						</IconButton>
+					}/>
+					<CardContent classes={{root: classes.chatContent}}>
+						{messages.map((value, index) => <Typography
+							variant="body2" paragraph key={index} classes={{paragraph: classes.chatParagraph}}
+						>
+							<strong>{value.sender}</strong> : {value.content}
+						</Typography>)}
+					</CardContent>
+					<CardActions>
+						<TextField variant="filled" size="small" fullWidth name="chatText" label="Message"/>
+						<Button color="primary">Send</Button>
+					</CardActions>
+				</Card>
+			</Zoom>
 		</main>
 	</Fragment>;
 };
