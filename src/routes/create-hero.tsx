7b885662e8cf0dca885b7e9082e4e3414a955903
Daniel Grondin <king@numsgil.co>
2020-01-13 15:21:55 -0500

    Tried fixing the stuff...

--- src/routes/create-hero.tsx
+++ src/routes/create-hero.tsx
@@ -1,25 +1,17 @@
 import {createElement, Fragment, FunctionComponent} from "react";
-import {
-	Card,
-	CardActionArea,
-	CardContent,
-	CardHeader,
-	createStyles,
-	Grid,
-	makeStyles,
-	Theme,
-	Typography
-} from "@material-ui/core";
+import {createStyles, Grid, makeStyles, Theme, Typography} from "@material-ui/core";
 
-/*const useStyles: makeStyles((theme: Theme){
+const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		content: {
 			backgroundImage: "url('/assets/create-hero-background')",
 		},
 	}),
-);*/
+);
 
 const CreateHero: FunctionComponent = () => {
+	const classes = useStyles();
+
 	return <Fragment>
 		<Grid container justify="center">
 			<Typography variant="h4">Create Hero</Typography>
