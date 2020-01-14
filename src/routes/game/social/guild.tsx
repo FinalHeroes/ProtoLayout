c42cf3b6a690ee2912420380149fb7614a95c387
Daniel Grondin <king@numsgil.co>
2020-01-14 14:07:51 -0500

    Marked guild and party as not implemented (they are optional features)

--- src/routes/game/social/guild.tsx
+++ src/routes/game/social/guild.tsx
@@ -4,6 +4,7 @@ import {Typography} from "@material-ui/core";
 const Guild: FunctionComponent = () => {
 	return <Fragment>
 		<Typography variant="h4">Guild</Typography>
+		<Typography variant="h6">Not Implemented</Typography>
 	</Fragment>;
 };
 
--- src/routes/game/social/party.tsx
+++ src/routes/game/social/party.tsx
@@ -4,6 +4,7 @@ import {Typography} from "@material-ui/core";
 const Party: FunctionComponent = () => {
 	return <Fragment>
 		<Typography variant="h4">Party</Typography>
+		<Typography variant="h6">Not Implemented</Typography>
 	</Fragment>;
 };
 
