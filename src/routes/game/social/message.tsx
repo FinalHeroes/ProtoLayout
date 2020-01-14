97e646de63e459625c9980fd092224a0dffabc03
Daniel Grondin <king@numsgil.co>
2020-01-14 13:55:15 -0500

    Added send date and sender to message list and added send dialog

--- src/routes/game/social/message.tsx
+++ src/routes/game/social/message.tsx
@@ -3,20 +3,47 @@ import {
 	Card,
 	CardContent,
 	CardHeader,
+	createStyles,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
 	ExpansionPanel,
 	ExpansionPanelActions,
-	ExpansionPanelDetails, ExpansionPanelProps,
+	ExpansionPanelDetails,
+	ExpansionPanelProps,
 	ExpansionPanelSummary,
 	Grid,
+	IconButton,
+	makeStyles,
+	TextField,
+	Theme,
 	Typography
 } from "@material-ui/core";
-import {ChangeEvent, createElement, FunctionComponent, Fragment, useState} from "react";
-import {ExpandMore} from "@material-ui/icons";
+import {ChangeEvent, createElement, Fragment, FunctionComponent, useState} from "react";
+import {CreateSharp, ExpandMoreSharp} from "@material-ui/icons";
+import {useForm} from "react-hook-form";
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		heading: {
+			fontSize: theme.typography.pxToRem(15),
+			flexBasis: "60%",
+			flexShrink: 0,
+		},
+		secondaryHeading: {
+			fontSize: theme.typography.pxToRem(15),
+			color: theme.palette.text.secondary,
+		},
+	}),
+);
 
 interface MessageData {
 	id: string;
 	title: string;
 	content: string;
+	sender: string;
+	receiver: string;
 	createdAt: Date;
 
 	previous?: MessageData;
@@ -27,11 +54,19 @@ interface MessageDisplayProps {
 }
 
 const MessageDisplay: FunctionComponent<MessageDisplayProps & ExpansionPanelProps> = props => {
+	const classes = useStyles();
 	const {message, ...expProps} = props;
 
 	return <ExpansionPanel {...expProps}>
-		<ExpansionPanelSummary expandIcon={<ExpandMore/>}>
-			<Typography>{message.title}</Typography>
+		<ExpansionPanelSummary expandIcon={<ExpandMoreSharp/>}>
+			<Typography className={classes.heading}>
+				{message.title}
+				&nbsp;
+				<Typography variant="caption">
+					from {message.sender}
+				</Typography>
+			</Typography>
+			<Typography className={classes.secondaryHeading}>{message.createdAt.toLocaleString()}</Typography>
 		</ExpansionPanelSummary>
 		<ExpansionPanelDetails>
 			<Typography>{message.content}</Typography>
@@ -61,7 +96,7 @@ const MessageDisplayList: FunctionComponent<MessageDisplayListProps> = props => 
 	return <Fragment>
 		{messages.map((msg, id) => (
 			<MessageDisplay message={msg} key={id}
-			                TransitionProps={{ unmountOnExit: true }}
+			                TransitionProps={{unmountOnExit: true}}
 			                expanded={expanded === msg.id}
 			                onChange={handleExpansion(msg.id)}
 			/>
@@ -70,21 +105,40 @@ const MessageDisplayList: FunctionComponent<MessageDisplayListProps> = props => 
 };
 
 const Message: FunctionComponent = () => {
+	const [sendOpen, setSendOpen] = useState(false);
+	const {register, handleSubmit, errors, clearError, reset} = useForm<MessageData>();
+
+	const handleClose = () => {
+		clearError();
+		reset();
+		setSendOpen(false);
+	};
+
+	const onSubmit = async (data: MessageData) => {
+		handleClose();
+	};
+
 	const messages: Array<MessageData> = [
 		{
 			id: "multi1",
 			title: "RE : RE : Multi Message",
 			content: "Blah Blah Blah",
+			sender: "System",
+			receiver: "",
 			createdAt: new Date(),
 			previous: {
 				id: "multi2",
 				title: "RE : Multi Message",
 				content: "Blah Blah Blah",
+				sender: "System",
+				receiver: "",
 				createdAt: new Date(),
 				previous: {
 					id: "multi3",
 					title: "Multi Message",
 					content: "Blah Blah Blah",
+					sender: "System",
+					receiver: "",
 					createdAt: new Date(),
 				},
 			},
@@ -95,17 +149,49 @@ const Message: FunctionComponent = () => {
 		id: value.toString(),
 		title: `Message ${value + 1}`,
 		content: "This is a Message!",
+		sender: `User ${value + 1}`,
+		receiver: "",
 		createdAt: new Date(),
 	}));
 
 	return <Grid container justify="center">
 		<Grid item lg={9}>
 			<Card raised>
-				<CardHeader title="Messages"/>
+				<CardHeader title="Messages" action={
+					<IconButton onClick={() => setSendOpen(true)}>
+						<CreateSharp/>
+					</IconButton>
+				}/>
 				<CardContent>
 					<MessageDisplayList messages={messages}/>
 				</CardContent>
 			</Card>
+
+			<Dialog open={sendOpen} onClose={handleClose} fullWidth maxWidth="md">
+				<DialogTitle>Send a new Message</DialogTitle>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<DialogContent>
+						<TextField margin="dense" label="To" name="receiver" fullWidth autoFocus
+						           error={Boolean(errors.receiver)}
+						           helperText={errors.receiver ? "The user entered is invalid or doesn't exist" : null}
+						           inputRef={register({
+							           required: true,
+							           validate: value => {
+								           return false;
+							           }
+						           })}
+						/>
+						<TextField margin="dense" label="Title" name="title" fullWidth
+						           error={Boolean(errors.title)} inputRef={register({required: true})}/>
+						<TextField margin="dense" label="Content" name="content" fullWidth multiline rows={4} rowsMax={10}
+						           error={Boolean(errors.content)} inputRef={register({required: true})}/>
+					</DialogContent>
+					<DialogActions>
+						<Button color="secondary" type="button" onClick={handleClose}>Cancel</Button>
+						<Button color="primary" type="submit">Send</Button>
+					</DialogActions>
+				</form>
+			</Dialog>
 		</Grid>
 	</Grid>;
 };
