ec82f726631958057d43aa4dec5188613c16b44f
Daniel Grondin <king@numsgil.co>
2020-01-14 11:21:46 -0500

    Added content to the messages page

--- src/routes/game/social/message.tsx
+++ src/routes/game/social/message.tsx
@@ -1,10 +1,109 @@
-import {createElement, Fragment, FunctionComponent} from "react";
-import {Typography} from "@material-ui/core";
+import {
+	Button,
+	Card,
+	CardContent,
+	CardHeader,
+	ExpansionPanel,
+	ExpansionPanelActions,
+	ExpansionPanelDetails, ExpansionPanelProps,
+	ExpansionPanelSummary,
+	Grid,
+	Typography
+} from "@material-ui/core";
+import {ChangeEvent, createElement, FunctionComponent, Fragment, useState} from "react";
+import {ExpandMore} from "@material-ui/icons";
+
+interface MessageData {
+	id: string;
+	title: string;
+	content: string;
+	createdAt: Date;
+
+	previous?: MessageData;
+}
+
+interface MessageDisplayProps {
+	message: MessageData;
+}
+
+const MessageDisplay: FunctionComponent<MessageDisplayProps & ExpansionPanelProps> = props => {
+	const {message, ...expProps} = props;
+
+	return <ExpansionPanel {...expProps}>
+		<ExpansionPanelSummary expandIcon={<ExpandMore/>}>
+			<Typography>{message.title}</Typography>
+		</ExpansionPanelSummary>
+		<ExpansionPanelDetails>
+			<Typography>{message.content}</Typography>
+		</ExpansionPanelDetails>
+		{message.previous && <ExpansionPanelDetails style={{display: "block"}}>
+			<MessageDisplay message={message.previous}/>
+		</ExpansionPanelDetails>}
+		<ExpansionPanelActions>
+			<Button color="secondary">Delete</Button>
+			<Button color="primary">Reply</Button>
+		</ExpansionPanelActions>
+	</ExpansionPanel>
+};
+
+interface MessageDisplayListProps {
+	messages: Array<MessageData>;
+}
+
+const MessageDisplayList: FunctionComponent<MessageDisplayListProps> = props => {
+	const {messages} = props;
+	const [expanded, setExpanded] = useState<string | false>(false);
+
+	const handleExpansion = (panel: string) => (event: ChangeEvent<{}>, isExpanded: boolean) => {
+		setExpanded(isExpanded ? panel : false);
+	};
+
+	return <Fragment>
+		{messages.map((msg, id) => (
+			<MessageDisplay message={msg} key={id}
+			                TransitionProps={{ unmountOnExit: true }}
+			                expanded={expanded === msg.id}
+			                onChange={handleExpansion(msg.id)}
+			/>
+		))}
+	</Fragment>;
+};
 
 const Message: FunctionComponent = () => {
-	return <Fragment>
-		<Typography variant="h4">Message</Typography>
-	</Fragment>;
+	const messages: Array<MessageData> = [
+		{
+			id: "multi1",
+			title: "RE : RE : Multi Message",
+			content: "Blah Blah Blah",
+			previous: {
+				id: "multi2",
+				title: "RE : Multi Message",
+				content: "Blah Blah Blah",
+				previous: {
+					id: "multi3",
+					title: "Multi Message",
+					content: "Blah Blah Blah",
+				},
+			},
+		},
+	];
+
+	[...Array(10).keys()].map(value => messages.push({
+		id: value.toString(),
+		title: `Message ${value + 1}`,
+		content: "This is a Message!",
+	}));
+
+	return <Grid container justify="center">
+		<Grid item lg={9}>
+			<Card raised>
+				<CardHeader title="Messages"/>
+				<CardContent>
+					<MessageDisplayList messages={messages}/>
+				</CardContent>
+			</Card>
+		</Grid>
+	</Grid>;
 };
 
 export default Message;
