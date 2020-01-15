eceb9a8a3c9258d87a152e0690c258ff5ba5937a
Daniel Grondin <king@numsgil.co>
2020-01-15 15:28:27 -0500

    Replaced sliders with up-down buttons and changed minimum values

--- src/routes/create-hero.tsx
+++ src/routes/create-hero.tsx
@@ -1,15 +1,19 @@
 import {
+	Button,
+	ButtonGroup,
 	Card,
+	CardActions,
 	CardContent,
 	CardHeader,
 	createStyles,
 	Grid,
 	makeStyles,
-	Slider,
 	Theme,
 	Typography
 } from "@material-ui/core";
 import {createElement, FunctionComponent, useState} from "react";
+import {AddSharp, RemoveSharp} from "@material-ui/icons";
+import {useNavigation} from "react-navi";
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -23,78 +27,92 @@ const useStyles = makeStyles((theme: Theme) =>
 			minHeight: "100vh",
 		},
 		card: {
+			position: "relative",
 			height: "100%",
-			backgroundColor: "rgba(255, 255, 255, 0.6)"
-		}
+			backgroundColor: "rgba(255, 255, 255, 0.6)",
+		},
 	}),
 );
 
 const CreateHero: FunctionComponent = () => {
 	const classes = useStyles();
-	const [str, setStr] = useState(1);
-	const [dex, setDex] = useState(1);
-	const [vit, setVit] = useState(1);
-	const [int, setInt] = useState(1);
+	const nav = useNavigation();
+	const [str, setStr] = useState(10);
+	const [dex, setDex] = useState(10);
+	const [vit, setVit] = useState(10);
+	const [int, setInt] = useState(10);
+
+	const getPointsLeft = () => 10 - (str - 10) - (dex - 10) - (vit - 10) - (int - 10);
 
 	return <div className={classes.content}>
 		<Card variant="outlined" classes={{root: classes.card}}>
 			<CardHeader title="Create your Hero"/>
 			<CardContent>
 				<Grid container direction="row" spacing={2}>
-					<Grid container item lg={6} direction="column" spacing={2}>
+					<Grid container item lg={3} direction="column" spacing={2}>
 						<Grid item lg>
-							<Typography gutterBottom>Stat Points Left</Typography>
-							<Slider
-								color="primary" min={1} max={10} marks disabled
-								value={10 - (str - 1) - (dex - 1) - (vit - 1) - (int - 1)}
-							/>
+							<Typography gutterBottom>Stat Points Left : {getPointsLeft()}</Typography>
 						</Grid>
 						<Grid item lg>
 							<Typography gutterBottom>Strength</Typography>
-							<Slider
-								min={1} max={6} valueLabelDisplay="auto"
-								value={str} marks
-								onChange={(e, value) => {
-									if (typeof value === "number")
-										setStr(value);
-								}}
-							/>
+							<ButtonGroup variant="contained" color="primary" fullWidth>
+								<Button onClick={() => setStr(str - 1)} disabled={str === 10}>
+									<RemoveSharp/>
+								</Button>
+								<Button disabled style={{color: "black"}}>
+									{str}
+								</Button>
+								<Button onClick={() => setStr(str + 1)} disabled={getPointsLeft() === 0}>
+									<AddSharp/>
+								</Button>
+							</ButtonGroup>
 						</Grid>
 						<Grid item lg>
 							<Typography gutterBottom>Dexterity</Typography>
-							<Slider
-								min={1} max={6} valueLabelDisplay="auto"
-								value={dex} marks
-								onChange={(e, value) => {
-									if (typeof value === "number")
-										setDex(value);
-								}}
-							/>
+							<ButtonGroup variant="contained" color="primary" fullWidth>
+								<Button onClick={() => setDex(dex - 1)} disabled={dex === 10}>
+									<RemoveSharp/>
+								</Button>
+								<Button disabled style={{color: "black"}}>
+									{dex}
+								</Button>
+								<Button onClick={() => setDex(dex + 1)} disabled={getPointsLeft() === 0}>
+									<AddSharp/>
+								</Button>
+							</ButtonGroup>
 						</Grid>
 						<Grid item lg>
 							<Typography gutterBottom>Vitality</Typography>
-							<Slider
-								min={1} max={6} valueLabelDisplay="auto"
-								value={vit} marks
-								onChange={(e, value) => {
-									if (typeof value === "number")
-										setVit(value);
-								}}
-							/>
+							<ButtonGroup variant="contained" color="primary" fullWidth>
+								<Button onClick={() => setVit(vit - 1)} disabled={vit === 10}>
+									<RemoveSharp/>
+								</Button>
+								<Button disabled style={{color: "black"}}>
+									{vit}
+								</Button>
+								<Button onClick={() => setVit(vit + 1)} disabled={getPointsLeft() === 0}>
+									<AddSharp/>
+								</Button>
+							</ButtonGroup>
 						</Grid>
 						<Grid item lg>
 							<Typography gutterBottom>Intellect</Typography>
-							<Slider
-								min={1} max={6} valueLabelDisplay="auto"
-								value={int} marks
-								onChange={(e, value) => {
-									if (typeof value === "number")
-										setInt(value);
-								}}
-							/>
+							<ButtonGroup variant="contained" color="primary" fullWidth>
+								<Button onClick={() => setInt(int - 1)} disabled={int === 10}>
+									<RemoveSharp/>
+								</Button>
+								<Button disabled style={{color: "black"}}>
+									{int}
+								</Button>
+								<Button onClick={() => setInt(int + 1)} disabled={getPointsLeft() === 0}>
+									<AddSharp/>
+								</Button>
+							</ButtonGroup>
 						</Grid>
 					</Grid>
-					<Grid container item lg={6} direction="column" spacing={2}>
+					<Grid item lg={6}>
+					</Grid>
+					<Grid container item lg={3} direction="column" spacing={2}>
 						<Grid item lg>
 							<Typography>Damage</Typography>
 						</Grid>
@@ -113,6 +131,17 @@ const CreateHero: FunctionComponent = () => {
 					</Grid>
 				</Grid>
 			</CardContent>
+			<CardActions style={{position: "absolute", bottom: 0, width: "100%"}}>
+				<Button
+					fullWidth
+					variant="contained"
+					color="primary"
+					disabled={getPointsLeft() > 0}
+					onClick={async () => await nav.navigate("/game")}
+				>
+					Create
+				</Button>
+			</CardActions>
 		</Card>
 	</div>;
 };
